ce8bb1d40bd8abdef3e6c1c2587daded2870adbd
Nathan Pettit <pettitnr1@gmail.com>
2022-10-11 12:24:48 -0600

    stability derivatives

--- project_2/code/project_2.jl
+++ project_2/code/project_2.jl
@@ -1,5 +1,5 @@
 using VortexLattice
-using Plots
+include("plots_default.jl")
 
 function wingEfficiency()
     ratios = 3:1:15
@@ -20,7 +20,22 @@ function wingEfficiency()
     plot(ratios, e, xlabel="Aspect Ratio", ylabel="Efficiency", label="")
 end
 
-function vortexLattice(request, ar=7.5)
+function stabilityDerivatives()
+    ratios = 0.003:0.001:0.008
+    n_r = length(ratios)
+    derivatives = zeros(n_r, 3)
+    index = 1
+    for r in ratios
+        derivatives[index, :] .= vortexLattice("derivatives", 7.5, r)
+        index = index + 1
+    end
+
+    d = plot(ratios, derivatives[:, 1], xlabel="Tail Volume Ratios", ylabel=L"\frac{d}{dx}C_d", label="")
+    l = plot(ratios, derivatives[:, 3], xlabel="Tail Volume Ratios", ylabel=L"\frac{d}{dx}C_\ell", label="")
+    plot(d, l)
+end
+
+function vortexLattice(request, ar=7.5, v_v=0.004978)
     coefficients = zeros(1, 2)
 
     # wing
@@ -80,6 +95,10 @@ function vortexLattice(request, ar=7.5)
     Omega = [0.0; 0.0; 0.0]
     fs = Freestream(Vinf, alpha, beta, Omega)
 
+    # tail volume ratio variables
+    S_v = 0.56
+    l_v = (v_v*Sref*bref)/S_v
+
     # construct surface
     grid, surface = wing_to_surface_panels(xle, yle, zle, chord, theta, phi, ns, nc;
         fc = fc, spacing_s=spacing_s, spacing_c=spacing_c)
@@ -87,17 +106,22 @@ function vortexLattice(request, ar=7.5)
     # generate surface panels for horizontal tail
     hgrid, htail = wing_to_surface_panels(xle_h, yle_h, zle_h, chord_h, theta_h, phi_h, ns_h, nc_h;
         mirror=mirror_h, fc=fc_h, spacing_s=spacing_s_h, spacing_c=spacing_c_h)
-    translate!(hgrid, [4.0, 0.0, 0.0])
-    translate!(htail, [4.0, 0.0, 0.0])
+    VortexLattice.translate!(hgrid, [l_v, 0.0, 0.0])
+    VortexLattice.translate!(htail, [l_v, 0.0, 0.0])
 
     # generate surface panels for vertical tail
     vgrid, vtail = wing_to_surface_panels(xle_v, yle_v, zle_v, chord_v, theta_v, phi_v, ns_v, nc_v;
         mirror=mirror_v, fc=fc_v, spacing_s=spacing_s_v, spacing_c=spacing_c_v)
-    translate!(vgrid, [4.0, 0.0, 0.0])
-    translate!(vtail, [4.0, 0.0, 0.0])
+    VortexLattice.translate!(vgrid, [l_v, 0.0, 0.0])
+    VortexLattice.translate!(vtail, [l_v, 0.0, 0.0])
 
     # create vector containing all surfaces
-    surfaces = [surface]
+    if request === "coefficients"
+        surfaces = [surface]
+    end
+    if request === "derivatives"
+        surfaces = [surface, htail, vtail]
+    end
 
     # we can use symmetry since the geometry and flow conditions are symmetric about the X-Z axis
     symmetric = true
@@ -112,18 +136,32 @@ function vortexLattice(request, ar=7.5)
     CDiff = far_field_drag(system)
 
     # retrieve stability derivatives
-    dCF, dCM = stability_derivatives(system)
+    dCFs, dCMs = stability_derivatives(system)
 
     CD, CY, CL = CF
     Cl, Cm, Cn = CM
 
-    dCD, dCY, dCL = dCF
-    dCl, dCm, dCn = dCM
+    CDa, CYa, CLa = dCFs.alpha
+    Cla, Cma, Cna = dCMs.alpha
+    CDb, CYb, CLb = dCFs.beta
+    Clb, Cmb, Cnb = dCMs.beta
+    CDp, CYp, CLp = dCFs.p
+    Clp, Cmp, Cnp = dCMs.p
+    CDq, CYq, CLq = dCFs.q
+    Clq, Cmq, Cnq = dCMs.q
+    CDr, CYr, CLr = dCFs.r
+    Clr, Cmr, Cnr = dCMs.r
 
     coefficients = [CL, CD]
 
+    derivatives = [CDa, CYa, CLa]
+    println(derivatives)
+
     if request === "coefficients"
         return coefficients
+    end
+    if request === "derivatives"
+        return derivatives
     end
 
 end
